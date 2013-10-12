19dd81a548140571a2ee11a3c8f73470d4e966cf
Martin Stadler <martin@siarp.de>
2013-10-12 14:33:40 +0200

    add view of first article

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 $.get('articles.json', function(data){
 	data = JSON.parse(data)
 	var articles = data.news
-	var templateString = document.getElementById('tpl-article').innerHTML
+	var templateString = document.getElementById('tpl-articles').innerHTML
 	var html = _.template(templateString, {articles: articles})
 	document.getElementById('articles').innerHTML = html
+
+	var firstArticle = articles[0]
+	var templateString = document.getElementById('tpl-article').innerHTML
+	var html = _.template(templateString, {article: firstArticle})
+	document.getElementById('article').innerHTML = html
 })
